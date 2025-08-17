b969315512268fe35f6d0127201981dc545c89a5
rparadiscsfoy <rparadis@csfoy.ca>
2025-08-17 15:09:58 -0400

    Correction de certaines inversions des lignes et des colonnes

--- TicTacToe/MainWindow.xaml.cs
+++ TicTacToe/MainWindow.xaml.cs
@@ -6,123 +6,123 @@ using System.Windows.Media.Imaging;
 using TicTacToeLib;
 namespace Exercice21
 {
-  public partial class MainWindow : Window
-  {
-    private const int SIZE = TicTacToe.SIZE;
-
-    private Image[,] images;
-    private char[,] grid;
-    private char currentPlayer;
-
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-      InitializeComponent();
+        private const int SIZE = TicTacToe.SIZE;
 
-      InitView();
-      InitModel();
-      UpdateView();
-    }
+        private Image[,] images;
+        private char[,] grid;
+        private char currentPlayer;
 
-    private void InitView()
-    {
-      var borderStyle = FindResource("GridLine") as Style;
-      var imageStyle = FindResource("GridImage") as Style;
-      var buttonTemplate = FindResource("GridButton") as ControlTemplate;
-
-      images = new Image[SIZE, SIZE];
-      for (var x = 0; x < SIZE; x++)
-      {
-        gameGrid.ColumnDefinitions.Add(new ColumnDefinition());
-        gameGrid.RowDefinitions.Add(new RowDefinition());
-
-        for (var y = 0; y < SIZE; y++)
+        public MainWindow()
         {
-          images[x, y] = CreateCell(gameGrid, y, x, borderStyle, buttonTemplate, imageStyle);
+            InitializeComponent();
+
+            InitView();
+            InitModel();
+            UpdateView();
         }
-      }
+
+        private void InitView()
+        {
+            var borderStyle = FindResource("GridLine") as Style;
+            var imageStyle = FindResource("GridImage") as Style;
+            var buttonTemplate = FindResource("GridButton") as ControlTemplate;
+
+            images = new Image[SIZE, SIZE];
+            for (var x = 0; x < SIZE; x++)
+            {
+                gameGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                gameGrid.RowDefinitions.Add(new RowDefinition());
+
+                for (var y = 0; y < SIZE; y++)
+                {
+                    images[x, y] = CreateCell(gameGrid, x, y, borderStyle, buttonTemplate, imageStyle);
+                }
+            }
+        }
+
+        private Image CreateCell(
+            Grid grid,
+            int x,
+            int y,
+            Style borderStyle,
+            ControlTemplate buttonTemplate,
+            Style imageStyle
+            )
+        {
+            var image = new Image();
+            image.Style = imageStyle;
+
+            var button = new Button();
+            button.Template = buttonTemplate;
+            button.Click += (_, _) => OnCellClick(x, y);
+
+            var innerGrid = new Grid();
+            innerGrid.Children.Add(image);
+            innerGrid.Children.Add(button);
+
+            var border = new Border();
+            border.BorderThickness = new Thickness
+            {
+                Top = y > 0 ? 3 : 0,
+                Right = x < SIZE - 1 ? 3 : 0,
+                Bottom = y < SIZE - 1 ? 3 : 0,
+                Left = x > 0 ? 3 : 0
+            };
+            border.Style = borderStyle;
+            border.Child = innerGrid;
+
+            Grid.SetColumn(border, x);
+            Grid.SetRow(border, y);
+            grid.Children.Add(border);
+
+            return image;
+        }
+
+        private void InitModel()
+        {
+            currentPlayer = TicTacToe.StartNewGame(SIZE, out grid);
+        }
+
+        private void UpdateView()
+        {
+            var ySize = grid.GetLength(0);
+            var xSize = grid.GetLength(1);
+
+            for (int x = 0; x < xSize; x++)
+                for (int y = 0; y < ySize; y++)
+                    images[x, y].Source = GetImage(grid[x, y]);
+        }
+
+        private void OnCellClick(int x, int y)
+        {
+            currentPlayer = TicTacToe.PutMarkAt(x, y, grid, currentPlayer);
+            UpdateView();
+
+            var winner = TicTacToe.FindWinner(grid);
+            if (winner != TicTacToe.NONE)
+            {
+                MessageBox.Show($"Le joueur {winner} a gagné!", "Félicications!", MessageBoxButton.OK, MessageBoxImage.Information);
+                currentPlayer = TicTacToe.StartNewGame(SIZE, out grid);
+                UpdateView();
+            }
+            else if (TicTacToe.IsGameADraw(grid))
+            {
+                MessageBox.Show($"Partie nulle.", "Oh. Dommage.", MessageBoxButton.OK, MessageBoxImage.Information);
+                currentPlayer = TicTacToe.StartNewGame(SIZE, out grid);
+                UpdateView();
+            }
+        }
+
+        private static ImageSource GetImage(char value)
+        {
+            return value switch
+            {
+                TicTacToe.CROSS => new BitmapImage(new Uri("/Images/Cross.png", UriKind.Relative)),
+                TicTacToe.CIRCLE => new BitmapImage(new Uri("/Images/Circle.png", UriKind.Relative)),
+                _ => null,
+            };
+        }
     }
-
-    private Image CreateCell(
-        Grid grid,
-        int x,
-        int y,
-        Style borderStyle,
-        ControlTemplate buttonTemplate,
-        Style imageStyle
-        )
-    {
-      var image = new Image();
-      image.Style = imageStyle;
-
-      var button = new Button();
-      button.Template = buttonTemplate;
-      button.Click += (_, _) => OnCellClick(x, y);
-
-      var innerGrid = new Grid();
-      innerGrid.Children.Add(image);
-      innerGrid.Children.Add(button);
-
-      var border = new Border();
-      border.BorderThickness = new Thickness
-      {
-        Top = y > 0 ? 3 : 0,
-        Right = x < SIZE - 1 ? 3 : 0,
-        Bottom = y < SIZE - 1 ? 3 : 0,
-        Left = x > 0 ? 3 : 0
-      };
-      border.Style = borderStyle;
-      border.Child = innerGrid;
-
-      Grid.SetColumn(border, x);
-      Grid.SetRow(border, y);
-      grid.Children.Add(border);
-
-      return image;
-    }
-
-    private void InitModel()
-    {
-      currentPlayer= TicTacToe.StartNewGame(SIZE, out grid);
-    }
-
-    private void UpdateView()
-    {
-      var ySize = grid.GetLength(0);
-      var xSize = grid.GetLength(1);
-
-      for (int x = 0; x < xSize; x++)
-        for (int y = 0; y < ySize; y++)
-          images[x, y].Source = GetImage(grid[x, y]);
-    }
-
-    private void OnCellClick(int x, int y)
-    {
-      currentPlayer = TicTacToe.PutMarkAt(y, x, grid, currentPlayer);
-      UpdateView();
-
-      var winner = TicTacToe.FindWinner(grid);
-      if (winner != TicTacToe.NONE)
-      {
-        MessageBox.Show($"Le joueur {winner} a gagné!", "Félicications!", MessageBoxButton.OK, MessageBoxImage.Information);
-        currentPlayer = TicTacToe.StartNewGame(SIZE, out grid);
-        UpdateView();
-      }
-      else if (TicTacToe.IsGameADraw (grid))
-      {
-        MessageBox.Show($"Partie nulle.", "Oh. Dommage.", MessageBoxButton.OK, MessageBoxImage.Information);
-        currentPlayer = TicTacToe.StartNewGame(SIZE, out grid);
-        UpdateView();
-      }
-    }
-
-    private static ImageSource GetImage(char value)
-    {
-      return value switch
-      {
-        TicTacToe.CROSS => new BitmapImage(new Uri("/Images/Cross.png", UriKind.Relative)),
-        TicTacToe.CIRCLE => new BitmapImage(new Uri("/Images/Circle.png", UriKind.Relative)),
-        _ => null,
-      };
-    }
-  }
 }
--- TicTacToeLib/TicTacToe.cs
+++ TicTacToeLib/TicTacToe.cs
@@ -1,31 +1,48 @@
-﻿namespace TicTacToeLib
+﻿using System;
+
+namespace TicTacToeLib
 {
 
-  public class TicTacToe
-  {
-    public const char NONE = ' ';
-    public const char CROSS = 'X';
-    public const char CIRCLE = 'O';
-    public const int SIZE = 3;
-
-    public static char StartNewGame(int size, out char[,] grid)
+    public class TicTacToe
     {
-      throw new System.NotImplementedException ();
-    }
+        public const char NONE = ' ';
+        public const char CROSS = 'X';
+        public const char CIRCLE = 'O';
+        public const int SIZE = 3;
 
-    public static char PutMarkAt(int column, int row, char[,] grid, char currentPlayer)
-    {
-      throw new System.NotImplementedException ();
-    }
+        public static char StartNewGame(int size, out char[,] grid)
+        {
+            // PROF : À COMPLÉTER. Les deux lignes suivantes sont erronnées, mais permettent la compilation.
+            // ATTENTION : le programme va tout de même planter immédiatement. Il devrait toutefois se lancer une fois que cette fonction (StartNewGame) sera complète.
+            // Vous devrez alors compléter les autres fonctions pour que le programme est le fonctionnement attendu.
+            grid = null;
 
-    public static char FindWinner(char[,] grid)
-    {
-      throw new System.NotImplementedException ();
-    }
+            return NONE;
+        }
 
-    public static bool IsGameADraw(char[,] grid)
-    {
-      throw new System.NotImplementedException ();
+        public static char PutMarkAt(int row, int column, char[,] grid, char currentPlayer)
+        {
+            // PROF : À COMPLÉTER. La ligne suivante est erronnée, mais permet la compilation.
+            
+            // PROF : n'oubliez pas -> cette fonction retourne le joueur suivant.
+            // - Si currentPlayer est CIRCLE, on devrait retourner CROSS
+            // - Si currentPlayer est CROSS, on devrait retourner CIRCLE
+            // Cette fonction comporte donc deux parties : 
+            // 1. Placer la bonne marque à la position demandée
+            // 2. Déterminer le prochain joueur selon le joueur courant (vous allez devoir trouver un moyen de faire l'inversion) et retourner le signe du prochain joueur.
+            return NONE;
+        }
+
+        public static char FindWinner(char[,] grid)
+        {
+            // PROF : À COMPLÉTER. La ligne suivante est erronnée, mais permet la compilation.
+            return NONE;
+        }
+
+        public static bool IsGameADraw(char[,] grid)
+        {
+            // PROF : À COMPLÉTER. La ligne suivante est erronnée, mais permet la compilation.
+            return false;
+        }
     }
-  }
 }
